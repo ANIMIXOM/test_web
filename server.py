5005157e5d9d625e2ebe231a1b5ce9e35aea0e50
ANIMIXOM <maximshlyamnev@yandex.ru>
2024-03-20 19:05:19 +0300

    Fix 3 release 2.0

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +8,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Привет от приложения Flask"
+
+
+@app.route("/vestertn")
+def user_hack():
+    ls = []
+    error = 0
+    while error < 200:
+        created_code = random.randint(10000, 99999)
+        if created_code in ls:
+            error += 1
+        else:
+            ls.append(created_code)
+    return ls
 
 
 if __name__ == '__main__':
